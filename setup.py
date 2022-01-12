6e299c2176f426d216366ee565a7c74415dea37a
Siddhartha Shandilya <siddharthashandilya104@gmail.com>
2022-01-12 15:00:36 +0530

    Initalize git repository

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project will build a machine learning model which predicts whether a wafer needs to be replaced or not based on inputs from various sensors.',
+    author='Siddhartha Shandilya',
+    license='MIT',
+)
